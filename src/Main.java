b47988181eb8ae0c26755a30f9c3bfe1fa8cad90
github <mmoloshova@gmail.com>
2022-12-28 21:25:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,137 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       /* Scanner sc = new Scanner(System.in);
+        Random ran = new Random();
+        int san = ran.nextInt(1,100);
+        System.out.println("write floor");
+        int fl= sc.nextInt();
+        System.out.println("weight");
+        int weight= sc.nextInt();
+        System.out.println(weight);
+        int n=1;
+        while(n<=fl) {
+            System.out.println(n + "floor");   //(n+"go")
+            n++;
+        }
+        for (int i = 1; i <=100; i++) {
+            System.out.println(i+"floor");
+        }*/
+//        Scanner scan = new Scanner(System.in);
+//        Random ran = new Random();
+//        System.out.println("Enter the floor you'd like to go to ==> ");
+//        int  fl = scan.nextInt();
+//        System.out.println("weight");
+//        int weight= scan.nextInt();
+//        int vera = ran.nextInt(1,4);
+//        if (fl > 100 || fl < 0 || weight>200 ) {
+//            System.out.println("Invalid selection");
+//        }
+//        else if (fl <= 100 && fl > 0 && weight<200  )  {
+//            for (int i = 1; i <= fl; i++)
+//                System.out.println(i+"floor");
+//            System.out.println(vera+ " lift is stuck");
+//            System.out.println("Ding!");
+//        }
+       // Scanner scan = new Scanner(System.in);
+
+
+
+//        int UnknownNumber, UserNumber, TrysCount = 0;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("ЗАДАЧА: Угадать число (от 0 до 100).");
+//        UnknownNumber = (int)Math.floor(Math.random() * 100);
+//        do {
+//            TrysCount++;
+//            System.out.print("Введите ваше число: ");
+//            UserNumber = input.nextInt();
+//            if (UserNumber > UnknownNumber)
+//                System.out.println("Моё число меньше.");
+//            else if (UserNumber < UnknownNumber) System.out.println("Моё число больше.");	else System.out.println("Вы угадали!");
+//        } while (UserNumber != UnknownNumber);
+//        System.out.println("Количество попыток: " + TrysCount);
+
+//    public static void main(String[] args) {
+//
+//        int sumn=0;
+//        int sump=0;
+//        for (int i = -100; i<=100; i++) {
+//            if (i < 0) {
+//                sumn += i;
+//            } else {
+//                sump += i;
+//            }
+//
+//        }
+//                System.out.println("Сумма отрицательных чиселЖ " + sumn + "\nСумма положительных чиселЖ " + sump);
+//
+//
+//
+//
+
+        //
+//        System.out.println(sum(9));
+//    }
+//
+//    static String sum (int a) {
+//        if (a % 2 == 0) {
+//            return("четный");
+//        } else {
+//            return("не четный");
+//        }
+//
+//    }
+//}
+
+//}
+//        System.out.println(meth("lkujbgdcff"));
+//    }
+//         static String meth(String name){
+//             System.out.println(67);
+//             return name;
+//         }
+//
+/**uib**/
+//        System.out.println(sum(8,6,6));
+//
+//    }
+//    static double sum(int a, int b,int c) {
+//        return (a+b+c)/3;
+//
+//    }
+//}
+
+
+//        System.out.println(san(5));
+//    }
+//    static int san(int a) {
+//        int sum=0;
+//        for (int i = 1; i < a; i++) {
+//            System.out.println(i);
+//            sum+=i;
+//        }
+//         return sum;
+
+
+//        int[] arr = new int[]{23, 4, 32, 34, 32, 23, 44, 33, 45};
+//        System.out.println(method(arr));
+//
+//    }
+//
+//    static int method(int[] array) {
+//        int min = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            min = Math.min(min, array[i]);
+//
+//        }
+//        return min;
+
+
+    }
+}
+
+
+
